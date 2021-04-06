1afaaf86914b20e0c2a8dc26a7472d09e8dd8638
Phil.Ganz <phil.ganz@noaa.gov>
2021-04-06 14:12:14 -0800

    convert partial to as.Date to avoid erroneous timestamps

--- 1_AR_data.R
+++ 1_AR_data.R
@@ -461,7 +461,7 @@ partial[, STRATA := ifelse(           # Define strata based on sample plan and g
   SAMPLE_PLAN %like% "Electronic Monitoring", paste("EM", GEAR, sep=" "), ifelse(
     SAMPLE_PLAN %like% "EM EFP" & GEAR == "TRW", "EM TRW EFP", ifelse(
       SAMPLE_PLAN %like% "Gear Type", GEAR, NA)))]
-partial <- unique(partial[, .(Effective_Date = EFFECTIVE_DATE, STRATA, Rate = RATE)])[order(Effective_Date, STRATA)]  # Run unique on simplified gear and sample plans
+partial <- unique(partial[, .(Effective_Date = as.Date(EFFECTIVE_DATE), STRATA, Rate = RATE)])[order(Effective_Date, STRATA)]  # Run unique on simplified gear and sample plans
 partial[, descriptions := partial_desc[partial, descriptions, on=.(STRATA)]]    # Merge descriptions in 
 partial[, formatted_strat := paste0("*", STRATA, "*")]                          # Create formatted_strata column
 partial[, txt := paste0(formatC(round(Rate * 100, 2), format='f', digits=2), '% in the ', formatted_strat, ' stratum')]    # Create txt column that combines Rate and formatted_strata
